6155d7d28043505cc454858dc834f55e2a392217
Jaime Martin <jmartinsportsnotes@gmail.com>
2013-06-21 19:00:17 +0100

    minMax

--- horseFrame.js
+++ horseFrame.js
@@ -1,77 +1,38 @@
+// FIND INDEX AND UPDATE IFRAME
+    var counter = 0;
+    var course = "red";
+    var allHorses = {}
+    var currentHorse = 2;
+    
+    var horseLinks =[];
+    var allLinks = [];
+    var allFilters=[];
 
-		//min max foreac horse
-		var overallMin = 2000;
-		var overallMAx = 0;
-		var theStart = true;
-		var xLabel = 'furlongs'
-		var xMin = 2000;
-		var xMax =0;
-		
-		
-		
-		var setMinAndMax = function(allTheHorses){
-		 	var min = 2000;
-			var max = 0;
-			
-		
-			
-			
-			 
-		 
-			for (var horseName in allTheHorses){
-			 
-			 
-		
-			 var horse = allTheHorses[horseName];
-			 
-			 horse['sName'] = horse['name'].replace(/\s/g, "");
-			 
-			 
+    function findIndex(arr,obj) {
+ 	var ind = arr.indexOf(obj);
+ 	if (ind > -1){
+		return ind;
+	} else {
+		return counter;
+	}
 
-				var races = horse['races'];
+}
 
-				
-				for (j = 0; j<races.length; j++){
+function updateIframe(index){
+     	
+     	
+    
+        if (index < 0){
+            index += horseLinks.length;
+        }
+        
+        counter = index;
+        
+        
 
-					var d = races[j]['rpr'];
-					var e = races[j][xLabel];
-					var s =races[j]['distNext'];
-					if (races[j]['pos']<2){
-						races[j]['dist']=parseFloat(s.slice(0,s.indexOf('L')));
-					} else {
-						races[j]['dist']=0-parseFloat(s.slice(0,s.indexOf('L')));
-					}
-					
-					
-					
-					
-					if (e < min){
-							xMin = e;
-						}
-						if (d > max){
-							xMax = e;
-						}
-
-					if (!isNaN(d)){
-	
-					 	d = parseInt(d);
-					 
-					 	races[j]['rpr'] = d;
-						if (d < min){
-							min = d;
-						}
-						if (d > max){
-							max = d;
-						}
-						
-					
-					}
-					
-				}
-			}
-			
-			
-			overallMin = min;
-			overallMax = max;
-		}
-		
+        var url = horseLinks[index%horseLinks.length]['href'];
+        
+        
+        document.getElementById('horseFrame').src = url;
+        return false;
+    }
---
+++ minMax.js
@@ -0,0 +1,77 @@
+
+		//min max foreac horse
+		var overallMin = 2000;
+		var overallMAx = 0;
+		var theStart = true;
+		var xLabel = 'furlongs'
+		var xMin = 2000;
+		var xMax =0;
+		
+		
+		
+		var setMinAndMax = function(allTheHorses){
+		 	var min = 2000;
+			var max = 0;
+			
+		
+			
+			
+			 
+		 
+			for (var horseName in allTheHorses){
+			 
+			 
+		
+			 var horse = allTheHorses[horseName];
+			 
+			 horse['sName'] = horse['name'].replace(/\s/g, "");
+			 
+			 
+
+				var races = horse['races'];
+
+				
+				for (j = 0; j<races.length; j++){
+
+					var d = races[j]['rpr'];
+					var e = races[j][xLabel];
+					var s =races[j]['distNext'];
+					if (races[j]['pos']<2){
+						races[j]['dist']=parseFloat(s.slice(0,s.indexOf('L')));
+					} else {
+						races[j]['dist']=0-parseFloat(s.slice(0,s.indexOf('L')));
+					}
+					
+					
+					
+					
+					if (e < min){
+							xMin = e;
+						}
+						if (d > max){
+							xMax = e;
+						}
+
+					if (!isNaN(d)){
+	
+					 	d = parseInt(d);
+					 
+					 	races[j]['rpr'] = d;
+						if (d < min){
+							min = d;
+						}
+						if (d > max){
+							max = d;
+						}
+						
+					
+					}
+					
+				}
+			}
+			
+			
+			overallMin = min;
+			overallMax = max;
+		}
+		
